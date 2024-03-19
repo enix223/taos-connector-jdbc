261e5d14738b2db1f3affe55fa265306a4ba9754
Enix Yu <enix223@163.com>
2024-03-19 17:46:16 +0800

    fix: unsigned type exceed range issue

--- src/main/java/com/taosdata/jdbc/rs/RestfulResultSet.java
+++ src/main/java/com/taosdata/jdbc/rs/RestfulResultSet.java
@@ -253,8 +253,6 @@ public class RestfulResultSet extends AbstractResultSet {
         if (value == null)
             return 0;
         long valueAsLong = Long.parseLong(value.toString());
-        if (valueAsLong == Byte.MIN_VALUE)
-            return 0;
         if (valueAsLong < Byte.MIN_VALUE || valueAsLong > Byte.MAX_VALUE)
             throwRangeException(value.toString(), columnIndex, Types.TINYINT);
 
@@ -275,8 +273,6 @@ public class RestfulResultSet extends AbstractResultSet {
         if (value == null)
             return 0;
         long valueAsLong = Long.parseLong(value.toString());
-        if (valueAsLong == Short.MIN_VALUE)
-            return 0;
         if (valueAsLong < Short.MIN_VALUE || valueAsLong > Short.MAX_VALUE)
             throwRangeException(value.toString(), columnIndex, Types.SMALLINT);
         return (short) valueAsLong;
@@ -291,8 +287,6 @@ public class RestfulResultSet extends AbstractResultSet {
         if (value == null)
             return 0;
         long valueAsLong = Long.parseLong(value.toString());
-        if (valueAsLong == Integer.MIN_VALUE)
-            return 0;
         if (valueAsLong < Integer.MIN_VALUE || valueAsLong > Integer.MAX_VALUE)
             throwRangeException(value.toString(), columnIndex, Types.INTEGER);
         return (int) valueAsLong;
@@ -321,8 +315,6 @@ public class RestfulResultSet extends AbstractResultSet {
         long valueAsLong = 0;
         try {
             valueAsLong = Long.parseLong(value.toString());
-            if (valueAsLong == Long.MIN_VALUE)
-                return 0;
         } catch (NumberFormatException e) {
             throwRangeException(value.toString(), columnIndex, Types.BIGINT);
         }
